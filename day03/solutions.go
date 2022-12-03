17ed50daf7cfcc003657d59ff0dbf175f4724a96
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-12-03 14:54:32 -0300

    day03 - part1

--- day03/solutions.go
+++ day03/solutions.go
@@ -1,9 +1,80 @@
 package day03
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Puzzle struct{}
 
+type Ruckpack struct {
+	half1 string
+	half2 string
+}
+
+func parseInput(raw string) []Ruckpack {
+	ruckpacks := strings.Split(strings.TrimSpace(raw), "\n")
+
+	result := []Ruckpack{}
+	for _, ruckpack := range ruckpacks {
+		ruckpack := Ruckpack{
+			half1: ruckpack[:len(ruckpack)/2],
+			half2: ruckpack[len(ruckpack)/2:],
+		}
+		result = append(result, ruckpack)
+	}
+
+	return result
+}
+
+func findRepeatedItem(s1, s2 string) string {
+	for _, c1 := range s1 {
+		for _, c2 := range s2 {
+			if c1 == c2 {
+				return string(c1)
+			}
+		}
+	}
+	panic("unreachable")
+}
+
+func convertItemToPriority(c string) int {
+	byteStr := []byte(c)
+	cAscii := int(byteStr[0])
+	if cAscii >= 97 && cAscii <= 122 {
+		cAscii -= 96
+	} else if cAscii >= 65 && cAscii <= 90 {
+		cAscii -= (65 - 27)
+	} else {
+		panic("unreachable (not a-z or A-Z)")
+	}
+	return cAscii
+}
+
 func (*Puzzle) Part1(input string) string {
-	return "-"
+	// lista mochiloes
+	//   lista de items
+	//     primeira metade | segunda metade
+	//
+	// identificar caracter repetido nas duas metades
+	// a:z == 1:26
+	// A:Z == 27:52
+
+	// 1. tratar o input: parseInput
+	//   1.2 separar lista em duas metades
+	// 2. encontrar o caracter repetido
+	// 3. converter o item em prioridade
+	// 4. somar as prioridades
+	ruckpacks := parseInput(input)
+
+	sum := 0
+	for _, ruckPack := range ruckpacks {
+		repeatedItem := findRepeatedItem(ruckPack.half1, ruckPack.half2)
+		priority := convertItemToPriority(repeatedItem)
+		sum += priority
+	}
+
+	return fmt.Sprint(sum)
 }
 
 func (*Puzzle) Part2(input string) string {
--- day03/solutions_test.go
+++ day03/solutions_test.go
@@ -6,13 +6,57 @@ import (
 )
 
 var p *Puzzle = &Puzzle{}
+var exampleInput string = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`
+
+func TestFindRepeatedItem(t *testing.T) {
+	tests := []struct {
+		s1       string
+		s2       string
+		Expected string
+	}{
+		{"abcdz", "efghz", "z"},
+		{"abcdz", "apoiy", "a"},
+		{"abcdzy", "lpoDry", "y"},
+	}
+
+	for i, tt := range tests {
+		solution := findRepeatedItem(tt.s1, tt.s2)
+		if solution != tt.Expected {
+			t.Errorf("[%02d] Expected %q, got %q", i, tt.Expected, solution)
+		}
+	}
+}
+
+func TestConvertItemToPriority(t *testing.T) {
+	tests := []struct {
+		c        string
+		Expected int
+	}{
+		{"a", 1},
+		{"z", 26},
+		{"A", 27},
+		{"Z", 52},
+	}
+
+	for i, tt := range tests {
+		solution := convertItemToPriority(tt.c)
+		if solution != tt.Expected {
+			t.Errorf("[%02d] Expected %d, got %d", i, tt.Expected, solution)
+		}
+	}
+}
 
 func TestPart1(t *testing.T) {
 	tests := []struct {
 		Input    string
 		Expected string
 	}{
-		{"", "-"},
+		{exampleInput, "157"},
 	}
 
 	for i, tt := range tests {
@@ -24,7 +68,7 @@ func TestPart1(t *testing.T) {
 
 	data, _ := ioutil.ReadFile("input.txt")
 	solution := p.Part1(string(data))
-	if solution != "-" {
+	if solution != "7727" {
 		t.Errorf("Solution for Part1: %s", solution)
 	}
 }
