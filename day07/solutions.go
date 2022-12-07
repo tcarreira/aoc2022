284cc4e14ec1cd8d3fd487fe521b9f48c6203aba
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-12-07 15:38:54 -0300

    day07 - part1
    Co-authored-by: Raphael Rossi <raphael.vieira.rossi@gmail.com>

--- day07/solutions.go
+++ day07/solutions.go
@@ -1,9 +1,123 @@
 package day07
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type Puzzle struct{}
 
+type FSNode struct {
+	parent   *FSNode
+	name     string
+	size     int
+	children map[string]*FSNode
+}
+
+func parseInput(raw string) FSNode {
+	filesystem := FSNode{
+		name:     "/",
+		children: make(map[string]*FSNode),
+	}
+	fsContext := &filesystem
+
+	lines := strings.Split(strings.TrimSpace(raw), "\n")
+	for _, line := range lines {
+		resultLine := strings.Split(line, " ")
+
+		if resultLine[0] == "$" {
+			// 0 = $    | $
+			// 1 = cd   | ls
+			// 2 = args | Panic
+			if resultLine[1] == "ls" {
+				continue
+			}
+
+			// / == root
+			// .. == parent
+			// abc == children
+			arg := resultLine[2]
+
+			switch arg {
+			case "/":
+				fsContext = &filesystem
+			case "..":
+				fsContext = fsContext.parent
+			default:
+				fsContext = fsContext.children[arg]
+			}
+			continue
+		}
+
+		newNode := fsContext.createNode(resultLine)
+		fsContext.children[newNode.name] = &newNode
+
+		// cd()
+	}
+
+	return filesystem
+}
+
+func (fsContext *FSNode) createNode(resultLine []string) FSNode {
+	if resultLine[0] == "dir" {
+		return FSNode{
+			name:     resultLine[1],
+			parent:   fsContext,
+			children: make(map[string]*FSNode),
+		}
+	}
+
+	size, err := strconv.Atoi(resultLine[0])
+	if err != nil {
+		panic(fmt.Errorf("not parsable: %w", err))
+	}
+	return FSNode{
+		name:     resultLine[1],
+		parent:   fsContext,
+		size:     size,
+		children: nil,
+	}
+}
+
+func (node *FSNode) computeSize() int {
+	for _, child := range node.children {
+		if len(child.children) == 0 {
+			node.size += child.size
+		} else {
+			node.size += child.computeSize()
+		}
+	}
+	return node.size
+}
+func (node *FSNode) getSumLessThan100000() int {
+	sum := 0
+	for _, child := range node.children {
+		if len(child.children) == 0 {
+			continue
+		} else {
+			sum += child.getSumLessThan100000()
+			if child.size <= 100000 {
+				sum += child.size
+			}
+		}
+	}
+	return sum
+}
+
 func (*Puzzle) Part1(input string) string {
-	return "-"
+	// filesystem := parseInput(input) -> FSNode
+	//     readCommand()
+	//     executeCommand()
+	//        fileNode := readFileNode(line) -> FSNode
+	//        filesystem.append(fileNode)
+	// dirSizes := computeDirSizes(filesystem)
+	// somar dir com tamanho <= 1000000
+	filesystem := parseInput(input)
+	filesystem.computeSize()
+	result := filesystem.getSumLessThan100000()
+
+	return fmt.Sprint(result)
 }
 
 func (*Puzzle) Part2(input string) string {
--- day07/solutions_test.go
+++ day07/solutions_test.go
@@ -6,13 +6,37 @@ import (
 )
 
 var p *Puzzle = &Puzzle{}
+var exampleInput = `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+`
 
 func TestPart1(t *testing.T) {
 	tests := []struct {
 		Input    string
 		Expected string
 	}{
-		{"", "-"},
+		{exampleInput, "95437"},
 	}
 
 	for i, tt := range tests {
