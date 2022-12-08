34a6422a9f1c752180d158e8f816dc5a37365571
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-12-08 12:03:07 -0300

    day 08 - part 1
    Co-authored-by: Ingrid Pacheco <ingridpacheco@users.noreply.github.com>

--- day08/solutions.go
+++ day08/solutions.go
@@ -1,9 +1,112 @@
 package day08
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type Puzzle struct{}
 
+// 30373
+// 25512
+// 65332
+// 33549
+// 35390
+
+// Input: 30373
+// PD   : OxxOx
+// top  : 7
+
+func parseInput(raw string) [][]int {
+	grid := [][]int{}
+
+	lines := strings.Split(strings.TrimSpace(raw), "\n")
+	for i, line := range lines {
+		grid = append(grid, []int{})
+		for _, c := range line {
+			h, err := strconv.Atoi(string(c))
+			if err != nil {
+				panic(fmt.Errorf("not parsable: %w", err))
+			}
+			grid[i] = append(grid[i], h)
+		}
+	}
+	return grid
+}
+
+func countVisible(grid [][]bool) int {
+	sum := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] {
+				sum += 1
+			}
+		}
+	}
+	return sum
+}
+
 func (*Puzzle) Part1(input string) string {
-	return "-"
+	grid := parseInput(input)
+	visibleGrid := checkVisible(grid)
+	sum := countVisible(visibleGrid)
+	return fmt.Sprint(sum)
+}
+
+func checkVisible(exampleGrid [][]int) [][]bool {
+	PD := [][]bool{}
+
+	for i, line := range exampleGrid {
+		PD = append(PD, []bool{})
+		top := -1
+		for _, c := range line {
+			if c > top {
+				top = c
+				PD[i] = append(PD[i], true)
+			} else {
+				PD[i] = append(PD[i], false)
+			}
+		}
+	}
+
+	for i, line := range exampleGrid {
+		top := -1
+		for j := len(line) - 1; j >= 0; j-- {
+			c := line[j]
+			if c > top {
+				top = c
+				PD[i][j] = true
+			}
+		}
+	}
+
+	for i := 0; i <= len(exampleGrid)-1; i++ {
+		line := exampleGrid[i]
+		top := -1
+		for j := 0; j <= len(line)-1; j++ {
+			c := exampleGrid[j][i]
+			// fmt.Println(i, j, c, top)
+			if c > top {
+				top = c
+				PD[j][i] = true
+			}
+		}
+	}
+
+	for i := len(exampleGrid) - 1; i >= 0; i-- {
+		line := exampleGrid[i]
+		top := -1
+		for j := len(line) - 1; j >= 0; j-- {
+			c := exampleGrid[j][i]
+			if c > top {
+				top = c
+				PD[j][i] = true
+			}
+		}
+	}
+
+	return PD
 }
 
 func (*Puzzle) Part2(input string) string {
--- day08/solutions_test.go
+++ day08/solutions_test.go
@@ -1,18 +1,25 @@
 package day08
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 )
 
 var p *Puzzle = &Puzzle{}
+var exampleInput = `30373
+25512
+65332
+33549
+35390
+`
 
 func TestPart1(t *testing.T) {
 	tests := []struct {
 		Input    string
 		Expected string
 	}{
-		{"", "-"},
+		{exampleInput, "21"},
 	}
 
 	for i, tt := range tests {
@@ -24,7 +31,7 @@ func TestPart1(t *testing.T) {
 
 	data, _ := os.ReadFile("input.txt")
 	solution := p.Part1(string(data))
-	if solution != "-" {
+	if solution != "1779" {
 		t.Errorf("Solution for Part1: %s", solution)
 	}
 }
@@ -48,5 +55,32 @@ func TestPart2(t *testing.T) {
 	solution := p.Part2(string(data))
 	if solution != "-" {
 		t.Errorf("Solution for Part2: %s", solution)
+	}
+}
+
+func TestPart3(t *testing.T) {
+
+	var exampleGrid = [][]int{
+		{3, 0, 3, 7, 3},
+		{2, 5, 5, 1, 2},
+		{6, 5, 3, 3, 2},
+		{3, 3, 5, 4, 9},
+		{3, 5, 3, 9, 0},
+	}
+	var resultGrid = [][]bool{
+		{true, true, true, true, true},
+		{true, true, true, false, true},
+		{true, true, false, true, true},
+		{true, false, true, false, true},
+		{true, true, true, true, true},
+	}
+
+	solution := checkVisible(exampleGrid)
+
+	expected, _ := json.Marshal(resultGrid)
+	got, _ := json.Marshal(solution)
+
+	if string(got) != string(expected) {
+		t.Errorf("Expected %q, got %q", string(expected), string(got))
 	}
 }
