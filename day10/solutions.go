f00a28ee6f82397b9866d0e64c6a01d79ebc32cf
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-12-10 21:24:29 -0300

    day10 - part1

--- day10/solutions.go
+++ day10/solutions.go
@@ -1,9 +1,77 @@
 package day10
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type Puzzle struct{}
 
+type Instruction struct {
+	command string
+	arg     int
+}
+
+func parseInput(raw string) []Instruction {
+	instructions := []Instruction{}
+	for _, line := range strings.Split(strings.TrimSpace(raw), "\n") {
+		parts := strings.Split(line, " ")
+		if len(parts) == 1 {
+			instructions = append(instructions, Instruction{command: "noop"})
+		} else {
+			arg, _ := strconv.Atoi(parts[1])
+			instructions = append(instructions, Instruction{"addx", arg})
+		}
+	}
+	return instructions
+}
+
+type State struct {
+	RegisterX         int
+	ClockCycle        int
+	Instructions      []Instruction
+	currentIntruction int
+}
+
+func (s *State) processInstructions(stopCycle int) (registerX int) {
+	for ; s.ClockCycle <= stopCycle; s.currentIntruction++ {
+		i := s.currentIntruction
+		if s.Instructions[i].command == "noop" {
+			if s.ClockCycle+1 >= stopCycle {
+				break
+			}
+			s.ClockCycle++
+		} else if s.Instructions[i].command == "addx" {
+			if s.ClockCycle+2 >= stopCycle {
+				break
+			}
+			s.ClockCycle += 2
+			s.RegisterX += s.Instructions[i].arg
+		}
+	}
+	return s.RegisterX
+}
+
 func (*Puzzle) Part1(input string) string {
-	return "-"
+	instructions := parseInput(input)
+	state := State{
+		RegisterX:    1,
+		ClockCycle:   0,
+		Instructions: instructions,
+	}
+
+	sum := 0
+	for stopCycle := 20; stopCycle <= 220; stopCycle += 40 {
+		registerX := state.processInstructions(stopCycle)
+		sum += (stopCycle * registerX)
+	}
+
+	// processInstructions()
+	//    ler cmd
+	//    verificar tick
+	//    atualizar X
+	return fmt.Sprint(sum)
 }
 
 func (*Puzzle) Part2(input string) string {
--- day10/solutions_test.go
+++ day10/solutions_test.go
@@ -6,13 +6,193 @@ import (
 )
 
 var p *Puzzle = &Puzzle{}
+var exampleInput = `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+`
+
+func TestProcessInstruction(t *testing.T) {
+	instructions := parseInput(`noop
+addx 3
+addx -5
+noop
+`)
+	state := State{
+		RegisterX:    1,
+		ClockCycle:   0,
+		Instructions: instructions,
+	}
+
+	tests := []struct {
+		stopCycle int
+		expected  int
+	}{
+		{1, 1},
+		{2, 1},
+		{3, 1},
+		{4, 4},
+		{5, 4},
+		{6, -1},
+	}
+
+	for _, tt := range tests {
+		got := state.processInstructions(tt.stopCycle)
+		if tt.expected != got {
+			t.Errorf("expected %d, got %d (cycle: %d)", tt.expected, got, tt.stopCycle)
+		}
+	}
+
+}
 
 func TestPart1(t *testing.T) {
 	tests := []struct {
 		Input    string
 		Expected string
 	}{
-		{"", "-"},
+		{exampleInput, "13140"},
 	}
 
 	for i, tt := range tests {
@@ -24,7 +204,7 @@ func TestPart1(t *testing.T) {
 
 	data, _ := os.ReadFile("input.txt")
 	solution := p.Part1(string(data))
-	if solution != "-" {
+	if solution != "16480" {
 		t.Errorf("Solution for Part1: %s", solution)
 	}
 }
