5a3f2e8fda38f59208e6060902e92c3108c3eb2b
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-12-15 14:47:15 -0300

    day15 - part1

--- day15/solutions.go
+++ day15/solutions.go
@@ -1,9 +1,87 @@
 package day15
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Puzzle struct{}
 
+// S = (3,2)
+// B = (6,5)
+// dist = 3+3 = 6
+// yDist = 7-2 = 5
+// xDist = 6-5 = 1
+// for 2,3,4
+// . . . . . . . . . .
+// . . . . . . . . . .
+// . . . S . . . . . .
+// . . . . . B . . . .
+// . . . . . . . . . .
+// . . . . . . . . . .
+// . . . . . . . . . .
+// . . # # # . . . . .  <==== 7
+// . . . . . . . . . .
+// . . . . . . . . . .
+
+type P struct {
+	x int
+	y int
+}
+type Sensor struct {
+	position P
+	beacon   P
+}
+
+func parseInput(raw string) []Sensor {
+	sensors := []Sensor{}
+	for _, line := range strings.Split(strings.TrimSpace(raw), "\n") {
+		var Sx, Sy, Bx, By int
+		fmt.Sscanf(line, "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d", &Sx, &Sy, &Bx, &By)
+		sensors = append(sensors, Sensor{P{Sx, Sy}, P{Bx, By}})
+	}
+	return sensors
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func manhattan(a, b P) int {
+	return abs(a.x-b.x) + abs(a.y-b.y)
+}
+
+func findCoverage(sensors []Sensor, y int) int {
+	coverage := make(map[int]bool)
+	for _, sensor := range sensors {
+		dist := manhattan(sensor.position, sensor.beacon)
+		yDist := abs(y - sensor.position.y)
+		xDist := dist - yDist // -2
+		for x := sensor.position.x - xDist; x <= sensor.position.x+xDist; x++ {
+			if sensor.beacon.y != y || x != sensor.beacon.x {
+				coverage[x] = true
+			}
+		}
+	}
+
+	return len(coverage)
+}
+
+func part1(input string, y int) string {
+	// parseInput() -> [](Sensor,Beacon)
+	// findCoverage(y=2000000)
+	//   - for pair(S,B) - dist()
+	//     - dist(S,x) <= dist
+	sensors := parseInput(input)
+	solution := findCoverage(sensors, y)
+	return fmt.Sprint(solution)
+}
+
 func (*Puzzle) Part1(input string) string {
-	return "-"
+	return part1(input, 2000000)
 }
 
 func (*Puzzle) Part2(input string) string {
--- day15/solutions_test.go
+++ day15/solutions_test.go
@@ -6,17 +6,31 @@ import (
 )
 
 var p *Puzzle = &Puzzle{}
+var exampleInput = `Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3`
 
 func TestPart1(t *testing.T) {
 	tests := []struct {
 		Input    string
 		Expected string
 	}{
-		{"", "-"},
+		{exampleInput, "26"},
 	}
 
 	for i, tt := range tests {
-		solution := p.Part1(tt.Input)
+		solution := part1(tt.Input, 10)
 		if solution != tt.Expected {
 			t.Errorf("[%02d] Expected %q, got %q", i, tt.Expected, solution)
 		}
@@ -24,7 +38,7 @@ func TestPart1(t *testing.T) {
 
 	data, _ := os.ReadFile("input.txt")
 	solution := p.Part1(string(data))
-	if solution != "-" {
+	if solution != "6275922" {
 		t.Errorf("Solution for Part1: %s", solution)
 	}
 }
