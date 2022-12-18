309f222113a63dfba6fa267a2b85d2e16775242b
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-12-18 17:23:25 -0300

    day18 - part1

--- day18/solutions.go
+++ day18/solutions.go
@@ -1,9 +1,63 @@
 package day18
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Puzzle struct{}
 
+type Cube struct {
+	x int
+	y int
+	z int
+}
+
+func parseInput(raw string) []Cube {
+	cubes := []Cube{}
+	for _, line := range strings.Split(strings.TrimSpace(raw), "\n") {
+		var x, y, z int
+		fmt.Sscanf(line, "%d,%d,%d", &x, &y, &z)
+		cubes = append(cubes, Cube{x, y, z})
+	}
+	return cubes
+}
+
+func buildCubesMap(cubes []Cube) (map[Cube]bool, int) {
+	cubesMap := map[Cube]bool{}
+	area := 0
+	for _, cube := range cubes {
+		cubesMap[cube] = true
+		area += 6
+
+		// check neighbors
+		if cubesMap[Cube{cube.x, cube.y, cube.z + 1}] { // up
+			area -= 2
+		}
+		if cubesMap[Cube{cube.x, cube.y, cube.z - 1}] { // down
+			area -= 2
+		}
+		if cubesMap[Cube{cube.x + 1, cube.y, cube.z}] { // front
+			area -= 2
+		}
+		if cubesMap[Cube{cube.x - 1, cube.y, cube.z}] { // back
+			area -= 2
+		}
+		if cubesMap[Cube{cube.x, cube.y + 1, cube.z}] { // left
+			area -= 2
+		}
+		if cubesMap[Cube{cube.x, cube.y - 1, cube.z}] { // right
+			area -= 2
+		}
+	}
+	return cubesMap, area
+}
+
 func (*Puzzle) Part1(input string) string {
-	return "-"
+	cubes := parseInput(input)
+	_, area := buildCubesMap(cubes)
+	// buildCubesMap(cubes) -> map[P]bool, area
+	return fmt.Sprint(area)
 }
 
 func (*Puzzle) Part2(input string) string {
--- day18/solutions_test.go
+++ day18/solutions_test.go
@@ -6,13 +6,28 @@ import (
 )
 
 var p *Puzzle = &Puzzle{}
+var exampleInput = `2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5
+`
 
 func TestPart1(t *testing.T) {
 	tests := []struct {
 		Input    string
 		Expected string
 	}{
-		{"", "-"},
+		{"1,1,1\n2,1,1", "10"},
+		{exampleInput, "64"},
 	}
 
 	for i, tt := range tests {
@@ -24,7 +39,7 @@ func TestPart1(t *testing.T) {
 
 	data, _ := os.ReadFile("input.txt")
 	solution := p.Part1(string(data))
-	if solution != "-" {
+	if solution != "4390" {
 		t.Errorf("Solution for Part1: %s", solution)
 	}
 }
