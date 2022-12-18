de8a83836b3f46f553a9c3a0d6f3a17776302d33
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-12-18 18:03:23 -0300

    day18 - part2

--- day18/solutions.go
+++ day18/solutions.go
@@ -53,6 +53,79 @@ func buildCubesMap(cubes []Cube) (map[Cube]bool, int) {
 	return cubesMap, area
 }
 
+type Node struct {
+	visited bool
+	isCube  bool
+}
+
+func buildMap3D(cubes []Cube) CubesMap {
+	myMap := [25][25][25]Node{}
+
+	for _, c := range cubes {
+		// if c.x == 0 || c.y == 0 || c.z == 0 {
+		// 	fmt.Println("DEBUG:::::", c)
+		// }
+		myMap[c.x+1][c.y+1][c.z+1].isCube = true // shift all to inside
+	}
+	return myMap
+}
+
+type CubesMap [25][25][25]Node
+
+func (m *CubesMap) dfsSearch(c Cube) {
+	m[c.x][c.y][c.z].visited = true
+	L := len(m)
+
+	// check neighbors
+	for _, p := range []Cube{
+		{c.x, c.y, c.z + 1},
+		{c.x, c.y, c.z - 1},
+		{c.x, c.y + 1, c.z},
+		{c.x, c.y - 1, c.z},
+		{c.x + 1, c.y, c.z},
+		{c.x - 1, c.y, c.z},
+	} {
+		if p.x < 0 || p.x >= L || p.y < 0 || p.y >= L || p.z < 0 || p.z >= L {
+			continue
+		}
+		if !m[p.x][p.y][p.z].isCube && !m[p.x][p.y][p.z].visited {
+			m.dfsSearch(p)
+		}
+	}
+}
+
+func (m *CubesMap) area() int {
+	area := 0
+	L := len(m)
+
+	for x := range m {
+		for y := range m[x] {
+			for z := range m[x][y] {
+				if !m[x][y][z].isCube {
+					continue
+				}
+
+				for _, p := range []Cube{
+					{x, y, z + 1},
+					{x, y, z - 1},
+					{x, y + 1, z},
+					{x, y - 1, z},
+					{x + 1, y, z},
+					{x - 1, y, z},
+				} {
+					if p.x < 0 || p.x >= L || p.y < 0 || p.y >= L || p.z < 0 || p.z >= L {
+						continue
+					}
+					if m[p.x][p.y][p.z].visited {
+						area += 1
+					}
+				}
+			}
+		}
+	}
+	return area
+}
+
 func (*Puzzle) Part1(input string) string {
 	cubes := parseInput(input)
 	_, area := buildCubesMap(cubes)
@@ -61,7 +134,13 @@ func (*Puzzle) Part1(input string) string {
 }
 
 func (*Puzzle) Part2(input string) string {
-	return "-"
+	cubes := parseInput(input)
+	cubesMap := buildMap3D(cubes)
+
+	cubesMap.dfsSearch(Cube{0, 0, 0})
+	area := cubesMap.area()
+
+	return fmt.Sprint(area)
 }
 
 func (*Puzzle) Notes() string {
