ded82ad0986cf8e2b7d3a1c86102e63dcd895888
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-12-23 15:32:30 -0300

    day23 - part2

--- day23/solutions_test.go
+++ day23/solutions_test.go
@@ -52,7 +52,7 @@ func TestPart2(t *testing.T) {
 		Input    string
 		Expected string
 	}{
-		{"", "-"},
+		{exampleInput, "20"},
 	}
 
 	for i, tt := range tests {
@@ -64,7 +64,7 @@ func TestPart2(t *testing.T) {
 
 	data, _ := os.ReadFile("input.txt")
 	solution := p.Part2(string(data))
-	if solution != "-" {
+	if solution != "986" {
 		t.Errorf("Solution for Part2: %s", solution)
 	}
 }
