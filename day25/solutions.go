0936d062916da62697f71477f067d14bff3fd69f
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-12-25 20:13:04 -0300

    day25 :D

--- day25/solutions.go
+++ day25/solutions.go
@@ -1,9 +1,76 @@
 package day25
 
+import "strings"
+
 type Puzzle struct{}
 
+func parseInput(raw string) []string {
+	return strings.Split(strings.TrimSpace(raw), "\n")
+}
+
+func exp(n, exp int) int {
+	res := 1
+	for i := 0; i < exp; i++ {
+		res *= n
+	}
+	return res
+}
+
+func snafuToDecimal(snafu string) int {
+	number := 0
+	for i := 0; i < len(snafu); i++ {
+		var digit int
+		switch string(snafu[len(snafu)-1-i]) {
+		case "2":
+			digit += 2
+		case "1":
+			digit += 1
+		case "0":
+			digit += 0
+		case "-":
+			digit -= 1
+		case "=":
+			digit -= 2
+		}
+		number += exp(5, i) * digit
+	}
+	return number
+}
+
+func decimalToSnafu(num int) string {
+	solution := ""
+	for num > 0 {
+		switch num % 5 {
+		case 4:
+			solution = "-" + solution
+			num += 2
+		case 3:
+			solution = "=" + solution
+			num += 3
+		case 2:
+			solution = "2" + solution
+		case 1:
+			solution = "1" + solution
+		case 0:
+			solution = "0" + solution
+		}
+		num /= 5
+	}
+
+	return solution
+}
+
 func (*Puzzle) Part1(input string) string {
-	return "-"
+	snafuList := parseInput(input)
+
+	sum := 0
+	for _, snafu := range snafuList {
+		x := snafuToDecimal(snafu)
+		sum += x
+	}
+
+	sol := decimalToSnafu(sum)
+	return sol
 }
 
 func (*Puzzle) Part2(input string) string {
--- day25/solutions_test.go
+++ day25/solutions_test.go
@@ -6,13 +6,62 @@ import (
 )
 
 var p *Puzzle = &Puzzle{}
+var exampleInput = `1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122`
+
+var decimalToSnafuMap = map[int]string{
+	1:         "1",
+	2:         "2",
+	3:         "1=",
+	4:         "1-",
+	5:         "10",
+	6:         "11",
+	7:         "12",
+	8:         "2=",
+	9:         "2-",
+	10:        "20",
+	15:        "1=0",
+	20:        "1-0",
+	2022:      "1=11-2",
+	12345:     "1-0---0",
+	314159265: "1121-1110-1=0",
+}
+
+func TestSnafuToDecimal(t *testing.T) {
+	for dec, snafu := range decimalToSnafuMap {
+		got := snafuToDecimal(snafu)
+		if got != dec {
+			t.Errorf("Got %d, expected %d", got, dec)
+		}
+	}
+}
+
+func TestDecimalToSnafu(t *testing.T) {
+	for dec, snafu := range decimalToSnafuMap {
+		got := decimalToSnafu(dec)
+		if got != snafu {
+			t.Errorf("Got %s, expected %s (dec: %d)", got, snafu, dec)
+		}
+	}
+}
 
 func TestPart1(t *testing.T) {
 	tests := []struct {
 		Input    string
 		Expected string
 	}{
-		{"", "-"},
+		{exampleInput, "2=-1=0"},
 	}
 
 	for i, tt := range tests {
