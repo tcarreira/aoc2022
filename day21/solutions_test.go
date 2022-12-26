0018433faed24f4ac725ae0891897c875a64f06e
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2022-12-26 11:20:30 -0300

    fix day21 solutions

--- day21/solutions_test.go
+++ day21/solutions_test.go
@@ -62,7 +62,7 @@ func TestPart2(t *testing.T) {
 
 	data, _ := os.ReadFile("input.txt")
 	solution := p.Part2(string(data))
-	if solution != "-" {
+	if solution != "3453748220116" {
 		t.Errorf("Solution for Part2: %s", solution)
 	}
 }
